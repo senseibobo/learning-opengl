183664df5d4ab3bc527f0409c5b25a8873958089
senseibobo <bogdanjsd@gmail.com>
2025-06-19 10:50:57 +0200

    rectangle

--- learning-opengl/main.cpp
+++ learning-opengl/main.cpp
@@ -93,9 +93,15 @@ int main() {
 
 
 	float vertices[] = {
+		0.5f, 0.5f, 0.0f,1.0f,1.0f,
+		0.5f, -0.5f, 0.0f,1.0f,0.0f,
 		-0.5f, -0.5f, 0.0f,0.0f,0.0f,
-		0.5f, -0.5f, 0.0f,0.0f,1.0f,
-		0.0f, 0.5f, 0.0f,1.0f,1.0f,
+		-0.5f, 0.5f, 0.0f,0.0f,1.0f,
+	};
+
+	int indices[] = {
+		0,1,3,
+		1,2,3
 	};
 
 
@@ -134,11 +140,16 @@ int main() {
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
+
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3*sizeof(float)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
 
@@ -150,8 +161,8 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
